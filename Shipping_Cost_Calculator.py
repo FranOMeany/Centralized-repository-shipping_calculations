63ad4ddf800f7e131a1fe1ce1f5c735ba3c112e8
Francisco J. O'Meany <francisco@omeany.net>
2024-01-26 01:56:16 -0500

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by FranOMeany (francisco@omeany.net)
+ # Here is a new update by FranOMeany (francisco@omeany.net) added more information for another pull req
